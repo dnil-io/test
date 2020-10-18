393c184f1810e94fc0cffcabf75be22fe2b3bd47
Daniel Omran <dotonesimple@gmail.com>
2020-10-18 16:53:28 +0200

    Updated flipcard

--- src/components/playground-example/playground-example.component.ts
+++ src/components/playground-example/playground-example.component.ts
@@ -1,30 +1,32 @@
-import {
-  Component,
-} from '@ribajs/core';
+import { Component } from "@ribajs/core";
 
-import template from './playground-example.component.html';
+import template from "./playground-example.component.html";
 
 interface Scope {
-  hello?: string;
+  open: boolean;
+  transitioning: boolean;
+  toggle: PlaygroundExampleComponent["toggle"];
 }
 
 export class PlaygroundExampleComponent extends Component {
-
-  public static tagName: string = 'rv-playground-example';
+  public static tagName: string = "rv-playground-example";
 
   protected autobind = true;
+  protected flipcardEl?: HTMLElement;
 
   static get observedAttributes() {
-    return ['hello'];
+    return [];
   }
 
   protected scope: Scope = {
-    hello: undefined,
+    open: false,
+    transitioning: false,
+    toggle: this.toggle,
   };
 
   constructor(element?: HTMLElement) {
     super(element);
-    console.debug('constructor', this);
+    console.debug("constructor", this);
   }
 
   protected connectedCallback() {
@@ -33,20 +35,18 @@ export class PlaygroundExampleComponent extends Component {
   }
 
   protected async init(observedAttributes: string[]) {
-    return super.init(observedAttributes)
-    .then((view) => {
+    return super.init(observedAttributes).then((view) => {
       return view;
     });
   }
 
   protected async beforeBind() {
     await super.beforeBind();
-    console.debug('beforeBind', this.scope);
   }
 
   protected async afterBind() {
     await super.afterBind();
-    console.debug('afterBind', this.scope);
+    this.flipcardEl = (this.el.getElementsByClassName("flip-card")[0] as HTMLElement);
   }
 
   protected requiredAttributes() {
@@ -55,6 +55,94 @@ export class PlaygroundExampleComponent extends Component {
 
   protected parsedAttributeChangedCallback(attributeName: string, oldValue: any, newValue: any, namespace: string | null) {
     return super.parsedAttributeChangedCallback(attributeName, oldValue, newValue, namespace);
+  }
+  
+  protected offset(el: HTMLElement) {
+    var rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+
+  protected toggle() {
+    if (this.scope.transitioning) {
+      return;
+    }
+    if (!this.scope.open) {
+      this.open();
+    } else {
+      this.close();
+    }
+  }
+
+  protected open() {
+    if (typeof this.flipcardEl === "undefined") {
+      throw new Error("this.flipcardEl cannot be undefined");
+    }
+    //transition to open
+    this.scope.transitioning = true;
+
+    var viewportOffset = this.offset(this.flipcardEl);
+    var top = viewportOffset.top;
+    var left = viewportOffset.left;
+
+    this.flipcardEl.style.transition = "none";
+    this.flipcardEl.style.position = "fixed";
+    this.flipcardEl.style.left = left + "px";
+    this.flipcardEl.style.top = top + "px";
+    this.flipcardEl.style.zIndex = "100000";
+    //blur(true);
+
+    setTimeout(() => {
+      if (typeof this.flipcardEl === "undefined") {
+        return;
+      }
+      this.flipcardEl.style.removeProperty("transition");
+      this.scope.open = true;
+
+      this.flipcardEl.addEventListener(
+        "transitionend",
+        () => {
+          if (typeof this.flipcardEl === "undefined") {
+            return;
+          }
+          this.scope.transitioning = false;
+        },
+        { once: true }
+      );
+    }, 1);
+  }
+
+  protected close() {
+    //transition to closed
+    if (typeof this.flipcardEl === "undefined") {
+      throw new Error("this.flipcardEl cannot be undefined");
+    }
+    this.scope.open = false;
+    this.scope.transitioning = true;
+
+    this.flipcardEl.addEventListener(
+      "transitionend",
+      () => {
+        if (typeof this.flipcardEl === "undefined") {
+          return;
+        }
+        this.flipcardEl.style.transition = "none";
+        this.flipcardEl.style.removeProperty("top");
+        this.flipcardEl.style.removeProperty("left");
+        this.flipcardEl.style.removeProperty("position");
+        this.flipcardEl.style.removeProperty("z-index");
+        this.scope.transitioning = false;
+
+        setTimeout(() => {
+          if (typeof this.flipcardEl === "undefined") {
+            return;
+          }
+          this.flipcardEl.style.removeProperty("transition");
+        });
+      },
+      { once: true }
+    );
   }
 
   // deconstructor
@@ -65,10 +153,8 @@ export class PlaygroundExampleComponent extends Component {
   protected template() {
     // Only set the component template if there no childs already
     if (this.el.hasChildNodes()) {
-      console.debug('Do not use template, because element has child nodes');
       return null;
     } else {
-      console.debug('Use template', template);
       return template;
     }
   }
--- src/ts/playground.module.ts
+++ src/ts/playground.module.ts
@@ -1,7 +1,7 @@
 import { RibaModule } from '@ribajs/core';
-import * as binders from './binders';
-import * as components from './components';
-import * as formatters from './formatters';
+import * as binders from '../binders';
+import * as components from '../components';
+import * as formatters from '../formatters';
 
 export const PlaygroundModule: RibaModule = {
   binders,
